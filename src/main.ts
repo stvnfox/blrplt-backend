51c709cadbb13e202f76a127c2f699ac72a6de18
steven.vos <steven.vos@iodigital.com>
2024-06-25 22:15:25 +0200

    feat(init): init swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // swagger setup
+  const config = new DocumentBuilder()
+    .setTitle('blrplt builder api')
+    .setDescription('the blrplt builder api documentation')
+    .setVersion('1.0')
+    .addBearerAuth(
+      { in: 'header', type: 'http', scheme: 'bearer', bearerFormat: 'JWT' },
+      'access-token',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  app.enableCors();
+  await app.listen(3210);
 }
 bootstrap();
