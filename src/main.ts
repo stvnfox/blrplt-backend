674471a5c536cec0c5b2eeb11276b376aa9a2132
steven.vos <steven.vos@iodigital.com>
2024-06-26 21:55:19 +0200

    feat(db): added connection to database and created a site table

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,22 @@
-import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core"
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger"
+import { AppModule } from "./app.module"
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule)
 
-  // swagger setup
-  const config = new DocumentBuilder()
-    .setTitle('blrplt builder api')
-    .setDescription('the blrplt builder api documentation')
-    .setVersion('1.0')
-    .addBearerAuth(
-      { in: 'header', type: 'http', scheme: 'bearer', bearerFormat: 'JWT' },
-      'access-token',
-    )
-    .build();
+    // swagger setup
+    const config = new DocumentBuilder()
+        .setTitle("blrplt builder api")
+        .setDescription("the blrplt builder api documentation")
+        .setVersion("1.0")
+        .addBearerAuth({ in: "header", type: "http", scheme: "bearer", bearerFormat: "JWT" }, "access-token")
+        .build()
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document);
+    const document = SwaggerModule.createDocument(app, config)
+    SwaggerModule.setup("docs", app, document)
 
-  app.enableCors();
-  await app.listen(3210);
+    app.enableCors()
+    await app.listen(3210)
 }
-bootstrap();
+bootstrap()
