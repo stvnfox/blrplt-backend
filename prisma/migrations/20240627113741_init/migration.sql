b05db809becaf64b15dff43185c9044ee92cc304
steven.vos <steven.vos@iodigital.com>
2024-06-27 14:09:24 +0200

    feat(init): add trigger and connection between tables and users

---
+++ prisma/migrations/20240627113741_init/migration.sql
@@ -0,0 +1,22 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('admin', 'user');
+
+-- CreateTable
+CREATE TABLE "profile" (
+    "id" UUID NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'user',
+
+    CONSTRAINT "profile_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "site" (
+    "id" UUID NOT NULL,
+    "name" TEXT NOT NULL,
+    "userId" UUID NOT NULL,
+
+    CONSTRAINT "site_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "site" ADD CONSTRAINT "site_userId_fkey" FOREIGN KEY ("userId") REFERENCES "profile"("id") ON DELETE CASCADE ON UPDATE CASCADE;
