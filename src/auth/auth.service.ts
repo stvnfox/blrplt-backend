80918f2193caf5fcbcefc46c2b2f9c72ad2bb19f
steven.vos <steven.vos@iodigital.com>
2024-08-18 16:20:24 +0200

    feat: add update password endpoint

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { JwtService } from "@nestjs/jwt"
 import { createClient, SupabaseClient } from "@supabase/supabase-js"
 
 import { CreateAuthDto } from "./dto/create-auth.dto"
+import { UpdatePasswordDto } from "./dto/update-password.dto"
 import { ResetPasswordDto } from "./dto/reset-password.dto"
 
 import { hashPassword } from "../../lib/passwords"
@@ -19,19 +20,19 @@ export class AuthService {
     async getUsers() {
         const { data, error } = await this.supabaseClient.auth.admin.listUsers({ page: 1, perPage: 1000 })
         if (error) {
-          this.logger.error(error)
-          throw new HttpException(
-            {
-              status: error.status,
-              message: error.message,
-            },
-            HttpStatus.BAD_REQUEST,
-            {
-              cause: error,
-            }
-          )
+            this.logger.error(error)
+            throw new HttpException(
+                {
+                    status: error.status,
+                    message: error.message,
+                },
+                HttpStatus.BAD_REQUEST,
+                {
+                    cause: error,
+                }
+            )
         }
-        
+
         const users = data.users || []
         return users
     }
@@ -39,7 +40,7 @@ export class AuthService {
     async register(createAuthDto: CreateAuthDto) {
         // check if user already exists and throw an error if they do
         const users = await this.getUsers()
-        if(users.find(user => user.email === createAuthDto.email)) {
+        if (users.find((user) => user.email === createAuthDto.email)) {
             throw new HttpException(
                 {
                     status: HttpStatus.CONFLICT,
@@ -93,10 +94,10 @@ export class AuthService {
                 }
             )
         }
-        
+
         // log the successful login for email
         this.logger.log(`User logged in successfully with email: ${loginAuthDto.email}`)
-        
+
         // sign the access token and return it
         this.jwtService.sign(data.session.access_token)
         return data.session.access_token
@@ -122,45 +123,63 @@ export class AuthService {
         this.logger.log(`User logged out successfully`)
     }
 
-    async resetPassword(ResetPasswordDto: ResetPasswordDto) {
-      const { error } = await this.supabaseClient.auth.resetPasswordForEmail(ResetPasswordDto.email, {
-        redirectTo: `${process.env.BASE_REDIRECT_URL}/update-password`,
-      })
+    async resetPassword(resetPasswordDto: ResetPasswordDto) {
+        const { error } = await this.supabaseClient.auth.resetPasswordForEmail(resetPasswordDto.email, {
+            redirectTo: `${process.env.BASE_REDIRECT_URL}/update-password`,
+        })
 
-      if (error) {
-        this.logger.error(error)
-        throw new HttpException(
-          {
-            status: error.status,
-            message: error.message,
-          },
-          HttpStatus.BAD_REQUEST,
-          {
-            cause: error,
-          }
-        )
-      }
+        if (error) {
+            this.logger.error(error)
+            throw new HttpException(
+                {
+                    status: error.status,
+                    message: error.message,
+                },
+                HttpStatus.BAD_REQUEST,
+                {
+                    cause: error,
+                }
+            )
+        }
 
-      return { message: "succeeded" }
+        this.logger.log(`Password reset email sent to ${resetPasswordDto.email}`)
+        return { message: "succeeded" }
     }
 
-    // create(createAuthDto: CreateAuthDto) {
-    //   return 'This action adds a new auth';
-    // }
+    async updatePassword(updatePasswordDto: UpdatePasswordDto) {
+        const { error } = await this.supabaseClient.auth.updateUser({
+            password: updatePasswordDto.password,
+        })
 
-    // findAll() {
-    //   return `This action returns all auth`;
-    // }
+        if (error) {
+            this.logger.error(error)
+            switch (error.code) {
+                case "same_password":
+                    throw new HttpException(
+                        {
+                            status: HttpStatus.CONFLICT,
+                            message: "new password should be different from the old password",
+                        },
+                        HttpStatus.CONFLICT,
+                        {
+                            cause: error,
+                        }
+                    )
+                default:
+                    throw new HttpException(
+                        {
+                            status: HttpStatus.BAD_REQUEST,
+                            message: "something went wrong",
+                        },
+                        HttpStatus.BAD_REQUEST,
+                        {
+                            cause: error,
+                        }
+                    )
+            }
+        }
 
-    // findOne(id: number) {
-    //   return `This action returns a #${id} auth`;
-    // }
-
-    // update(id: number, updateAuthDto: UpdateAuthDto) {
-    //   return `This action updates a #${id} auth`;
-    // }
-
-    // remove(id: number) {
-    //   return `This action removes a #${id} auth`;
-    // }
+        this.logger.log(`Password updated successfully`)
+        return { message: "your password is succesfully changed!" }
+    }
 }
